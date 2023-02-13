c894b1fc0fd0c0235596e94ae2dec5778c3e4dd8
Byron-Myo <mthihak.1996@gmail.com>
2023-02-13 16:32:23 +0630

    Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './Create';
 import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
 
   return (
-    <div className="App">
-    <Navbar />
-      <div className="content">
-        <Home/>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/useFetch.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+const useFetch=(url) => {
+
+
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+    useEffect(() => {
+      setTimeout(() => {
+        fetch(url)
+          .then((res) => {
+            // console.log(res);
+            if (!res.ok) {
+              throw Error("could not fetch the data for the resource");
+            }
+            return res.json();
+          })
+          .then((data) => {
+            setData(data);
+            setIsPending(false);
+            setError(null);
+          })
+          .catch((err) => {
+            // console.log(err.message);
+            setIsPending(false);
+            setError(err.message);
+          });
+      }, 1000);
+    }, []);
+
+    return {data, isPending, error}
+}
+
+export default useFetch;
